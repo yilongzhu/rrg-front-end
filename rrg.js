1cfd0c345b1310b39b2418f1af1ae842d7a8a078
Yilong Zhu <me@yilongzhu.com>
2019-04-24 23:36:02 -0400

    add Yelp display requirements

--- rrg.js
+++ rrg.js
@@ -44,19 +44,51 @@ function getPrice() {
         function() {return this.value;}).get().join(",");
 }
 
+function getRating(rating) {
+    switch(rating) {
+        case 1:
+            return "images/yelp/regular_1.png";
+        case 1.5:
+            return "images/yelp/regular_1_half.png";
+        case 2:
+            return "images/yelp/regular_2.png";
+        case 2.5:
+            return "images/yelp/regular_2_half.png";
+        case 3:
+            return "images/yelp/regular_3.png";
+        case 3.5:
+            return "images/yelp/regular_3_half.png";
+        case 4:
+            return "images/yelp/regular_4.png";
+        case 4.5:
+            return "images/yelp/regular_4_half.png";
+        case 5:
+            return "images/yelp/regular_5.png";
+        default:
+            return "images/yelp/regular_0.png";
+    }
+}
+
 var apiURL = "https://yilongzhu.com:8443/rrg";
 var nm = document.getElementById("name");
 var addr = document.getElementById("address");
 var dir = document.getElementById("directions");
-var img = document.getElementById("place")
+var place = document.getElementById("place");
+var url = document.getElementById("yelp-url");
+var ratingImg = document.getElementById("rating").getElementsByTagName("img")[0];
+var reviews = document.getElementById("rating").getElementsByTagName("span")[0];
+
 
 function getRestaurant(lat, lng, rad, price) {
     $.get(apiURL , {latitude: lat, longitude: lng, radius: rad, price: price}, data => {
         var address = data.address.join(", ");
         var mapsLink = "https://www.google.com/maps/dir/?api=1&destination=" + encodeURIComponent(address);
-        img.style.backgroundImage = "url(" + data.image_url + ")";
+        place.style.backgroundImage = "url(" + data.image_url + ")";
         nm.innerHTML = data.name;
         addr.innerHTML = address;
         dir.href = mapsLink;
+        ratingImg.src = getRating(data.rating);
+        reviews.innerHTML = data.review_count + " Reviews";
+        url.href = data.url;
     }, "json");
 }
